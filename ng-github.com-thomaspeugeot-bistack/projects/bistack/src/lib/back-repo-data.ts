da88ff4cd7593634d494d1fedb1f4f628c7073b3
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-02-16 11:35:54 +0100

    new gongc with Component Index

--- ng-github.com-thomaspeugeot-bistack/projects/bistack/src/lib/back-repo-data.ts
+++ ng-github.com-thomaspeugeot-bistack/projects/bistack/src/lib/back-repo-data.ts
@@ -9,11 +9,14 @@ export class BackRepoData {
 	FooAPIs = new Array<FooAPI>()
 
 
+	// index of the web socket for this stack type (unique among all stack instances)
+	GONG__Index : number
 
 	constructor(data?: Partial<BackRepoData>) {
 		// insertion point for copies
 		this.FooAPIs = data?.FooAPIs || [];
 
+		this.GONG__Index = data?.GONG__Index ?? -1;   // Assign Index here
 	}
 
 }
--- otherstack/ng-github.com-thomaspeugeot-bistack-otherstack/projects/otherstack/src/lib/back-repo-data.ts
+++ otherstack/ng-github.com-thomaspeugeot-bistack-otherstack/projects/otherstack/src/lib/back-repo-data.ts
@@ -9,11 +9,14 @@ export class BackRepoData {
 	BarAPIs = new Array<BarAPI>()
 
 
+	// index of the web socket for this stack type (unique among all stack instances)
+	GONG__Index : number
 
 	constructor(data?: Partial<BackRepoData>) {
 		// insertion point for copies
 		this.BarAPIs = data?.BarAPIs || [];
 
+		this.GONG__Index = data?.GONG__Index ?? -1;   // Assign Index here
 	}
 
 }
